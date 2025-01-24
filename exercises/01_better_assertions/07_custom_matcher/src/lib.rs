8e6e86d3644bb933c952004ace830d0b44f636dc
picaroloco <pic4roloco@gmail.com>
2025-01-24 18:48:59 +0100

    feat: Implement custom  matcher for HTTP status codes
    - Added a matcher to verify if a StatusCode is a redirect (3xx).
    - Included test cases for both success and failure scenarios.
    - Improved error messages for unmatched cases.

--- exercises/01_better_assertions/07_custom_matcher/src/lib.rs
+++ exercises/01_better_assertions/07_custom_matcher/src/lib.rs
@@ -2,8 +2,26 @@
 use googletest::matcher::Matcher;
 use http::StatusCode;
 
-pub fn is_redirect() -> impl Matcher<StatusCode> {
-    todo!()
+pub fn is_redirect() -> impl Matcher<ActualT = StatusCode> {
+    struct Redirect(StatusCode);
+
+    impl Matcher for Redirect
+    {
+        type ActualT = StatusCode;
+    
+        fn matches(&self, actual: &Self::ActualT) -> googletest::matcher::MatcherResult {
+            (self.0 == *actual).into()
+        }
+    
+        fn describe(&self, matcher_result: googletest::matcher::MatcherResult) -> googletest::description::Description {
+            match matcher_result {
+                googletest::matcher::MatcherResult::Match => format!("is a redirection status code").into(),
+                googletest::matcher::MatcherResult::NoMatch => format!("isn't a redirection status code").into(),
+            }
+        }
+    }
+    // return instance
+    Redirect(StatusCode::MOVED_PERMANENTLY)
 }
 
 #[cfg(test)]
